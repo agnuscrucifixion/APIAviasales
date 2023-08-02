d8c623b146e22b5a46ac3b400788f07e44800946
agnuscrucifixion <nikitabysko123@gmail.com>
2023-08-02 12:01:39 +0300

    Restoring an old project

---
+++ Avisales/src/main/java/ru/padwicki/aviasales/app/Application.java
@@ -0,0 +1,20 @@
+package ru.padwicki.aviasales.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
+
+@EnableJpaRepositories(basePackages = "ru.padwicki.aviasales.domain.repository")
+@EntityScan("ru.padwicki.aviasales.domain.entity")
+@ComponentScan("ru.padwicki.aviasales")
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class);
+    }
+}
